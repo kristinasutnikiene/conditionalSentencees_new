cf8413a73554971dff3ccbd7f0c72938911e27b4
kristinasutnikiene <kristina.sutnikiene@gmail.com>
2024-09-24 19:25:13 +0300

    pirmas kartas

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
-print("hi")
+#2. Liepkite vartotojui suvesti savo amžių. Patikrinkite ar amžius yra didesnis
+#arba lygus 18-ai, jei taip -išveskite "jūs galite balsuoti".
 
-if 7:
-    print("belekas")
+# amzius = int(input('Kiek jums metu?'))
+# if  amzius >= 18:
+#     print('Jus galite balsuoti!')
+# else:
+#     print('Jus negalite balsuoti :(')
+
+# 3. Leiskite vartotojui suvesti norimą kiekį pažymių (pavyzdžiui, jūs nusimatote 3-is kintamuosius,
+# tai leidžiate padaryti 3 įvedimus). Raskite šių pažymių vidurkį.
+# Patikrinkite ar vidurkis teigiamas (daugiau arba lygu 5-iems), jei taip -išveskite "vidurkis teigiamas".
+
+# print ('Iveskite 4 pazymius:')
+# Mat = int(input("Matematika "))
+# Liet = int(input('Lietuviu kalba '))
+# Fiz = int(input('Fizika '))
+# Ang = int(input('Anglu kalba '))
+#
+# Vid = (Mat+Liet+Fiz+Ang)/4
+# print (f'Vidurkis: {Vid}')
+#
+# if Vid  >= 5:
+#     print('Vidurkis yra teigiamas!')
+# else:
+#     print('Liekate kartoti kurso!')
+
+# 4. Susikurkite skaičiaus kintamąjį arba leiskite šį skaičių įvesti vartotojui.
+# Atlikite šiuos patikrinimus ir veiksmus:
+# 1. Jei skaičius dalinasi iš 5, tuomet išveskite šio skaičiaus daugybos lentelę nuo 1 iki 5.
+# 2. Jei skaičius lyginis, tuomet išveskite šį skaičių, jo kvadratą ir jį padalintą iš 2.
+# 3. Jei skaičius nesidalina iš 7, tuomet susikurkite antrąjį kintamąjį, išveskite šių dviejų
+# skaičių sumą, skirtumą, sandaugą, dalmenį.
+
